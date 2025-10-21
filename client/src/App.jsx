598e57b34fbf916718b801f29da12f64823fd018
AhmedDeveloper14 <itechahmed1014@gmail.com>
2025-10-22 03:09:30 +0500

    Testimonial Star Rating and  Newsletter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
       {!isOwnerPath && <Navbar />}
       <div className='min-h-[70vh]'>
       <Routes>
-        <Route path='/' element={<Home/>} /> 
+        <Route path='/' element={<Home/>} />
       </Routes>
       </div>
     </div>
